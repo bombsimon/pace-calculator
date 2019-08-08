43d228955bebbfa2a7be0b2157c36f84257eaf25
Simon Sawert <simon@sawert.se>
2019-08-08 22:34:09 +0200

    Lint the code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TimeField from 'react-simple-timefield';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import TimeField from "react-simple-timefield";
+import PropTypes from "prop-types";
+import "./index.css";
 
 class ConverterBar extends React.Component {
   constructor(props) {
@@ -12,36 +13,47 @@ class ConverterBar extends React.Component {
   }
 
   handleChange(e) {
-    this.props.onChange(e);
+    const { onChange } = this.props;
+
+    onChange(e);
   }
 
   handleTimeChange(time) {
-    this.props.onTimeChange(time);
+    const { onTimeChange } = this.props;
+
+    onTimeChange(time);
   }
 
   render() {
+    const { time, distance } = this.props;
+
     return (
       <div className="converter-bar">
-        <div claassName="field">
-          <label>Time (HH:MM:SS)</label>
-          <TimeField
-              value={this.props.time}
+        <div className="field">
+          <label htmlFor="time-input">
+            Time (HH:MM:SS)
+            <TimeField
+              value={time}
               onChange={this.handleTimeChange}
+              input={<input id="time-input" />}
               showSeconds
-          />
+            />
+          </label>
         </div>
 
         <div claassName="field">
-          <label>Distance (km as decimal value)</label>
-          <input
-            id="distance-input"
-            type="number"
-            name="distance"
-            step="0.1"
-            placeholder="Distance"
-            value={this.props.distance}
-            onChange={this.handleChange}
-          />
+          <label htmlFor="distance-input">
+            Distance (km as decimal value)
+            <input
+              id="distance-input"
+              type="number"
+              name="distance"
+              step="0.1"
+              placeholder="Distance"
+              value={distance}
+              onChange={this.handleChange}
+            />
+          </label>
         </div>
       </div>
     );
@@ -50,20 +62,23 @@ class ConverterBar extends React.Component {
 
 class ResultTable extends React.Component {
   calculateSpeedAndPace() {
-    let [ hour, minute, second ] = this.props.time.split(":");
+    const { time, distance } = this.props;
 
-    let fractions_of_minute = parseInt(second) / 60;
-    let fractions_of_hour = ( parseFloat(minute) + fractions_of_minute ) / 60;
-    let hours_with_fractions = Number(hour) + fractions_of_hour;
+    const [hour, minute, second] = time.split(":");
 
-    let speed_kph = this.props.distance / hours_with_fractions;
-    let pace = hours_with_fractions / this.props.distance * 60;
+    const fractionOfMinute = Number(second) / 60;
+    const fractionOfHour = (parseFloat(minute) + fractionOfMinute) / 60;
+    const hoursWithFraction = Number(hour) + fractionOfHour;
 
-    return { speed: speed_kph, pace: pace };
+    const speed = distance / hoursWithFraction;
+    const pace = (hoursWithFraction / distance) * 60;
+
+    return { speed, pace };
   }
 
   render() {
-    let speedAndPace = this.calculateSpeedAndPace();
+    const speedAndPace = this.calculateSpeedAndPace();
+    const { time, distance } = this.props;
 
     return (
       <table>
@@ -77,8 +92,8 @@ class ResultTable extends React.Component {
         </thead>
         <tbody>
           <tr>
-            <td>{this.props.time}</td>
-            <td>{parseFloat(this.props.distance).toFixed(2)} km</td>
+            <td>{time}</td>
+            <td>{parseFloat(distance).toFixed(2)} km </td>
             <td>{parseFloat(speedAndPace.speed).toFixed(2)} km/h</td>
             <td>{parseFloat(speedAndPace.pace).toFixed(2)} min/km</td>
           </tr>
@@ -96,7 +111,7 @@ class PaceCalculator extends React.Component {
       distance: 7.5,
       time: "00:45:00"
     };
-    
+
     this.handleChange = this.handleChange.bind(this);
     this.handleTimeChange = this.handleTimeChange.bind(this);
   }
@@ -106,30 +121,50 @@ class PaceCalculator extends React.Component {
   }
 
   handleTimeChange(time) {
-    this.setState({ time: time });
+    this.setState({ time });
   }
 
   render() {
+    const { distance, time } = this.state;
+
     return (
       <div>
         <ConverterBar
-          distance={this.state.distance}
-          time={this.state.time}
+          distance={distance}
+          time={time}
           onChange={this.handleChange}
           onTimeChange={this.handleTimeChange}
         />
-        <ResultTable
-          distance={this.state.distance}
-          time={this.state.time}
-        />
+        <ResultTable distance={distance} time={time} />
       </div>
     );
   }
 }
 
-ReactDOM.render(
-  <PaceCalculator />,
-  document.getElementById('root')
-);
+ConverterBar.propTypes = {
+  time: PropTypes.string,
+  distance: PropTypes.number,
+  onChange: PropTypes.func,
+  onTimeChange: PropTypes.func
+};
+
+ConverterBar.defaultProps = {
+  time: "",
+  distance: 0.0,
+  onChange: () => undefined,
+  onTimeChange: () => undefined
+};
+
+ResultTable.propTypes = {
+  time: PropTypes.string,
+  distance: PropTypes.number
+};
+
+ResultTable.defaultProps = {
+  time: "",
+  distance: 0.0
+};
+
+ReactDOM.render(<PaceCalculator />, document.getElementById("root"));
 
 // vim: set ts=2 sw=2 et:
