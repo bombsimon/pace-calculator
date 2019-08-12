3c908137e655b6e55de58442d6da968d816ff160
Simon Sawert <simon@sawert.se>
2019-08-12 10:32:01 +0200

    Cleanup, move slider to separate file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React, { Component, Fragment } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import TimeField from "react-simple-timefield";
 import PropTypes from "prop-types";
 import { Slider, Rail, Handles, Tracks } from "react-compound-slider";
+import { SliderRail, Handle, Track } from "./slider";
 import "./index.css";
-
-const sliderStyle = {
-  position: "relative",
-  width: "100%",
-  touchAction: "none"
-};
-
-const domain = [0, 42.2];
 
 class ConverterBar extends React.Component {
   constructor(props) {
@@ -47,7 +40,7 @@ class ConverterBar extends React.Component {
 
   render() {
     const { time, distance } = this.props;
-
+    const domain = [0, 42.2];
     const values = [distance];
 
     return (
@@ -82,7 +75,11 @@ class ConverterBar extends React.Component {
             mode={2}
             step={0.1}
             domain={domain}
-            rootStyle={sliderStyle}
+            rootStyle={{
+              position: "relative",
+              width: "100%",
+              touchAction: "none"
+            }}
             onUpdate={this.onUpdate}
             onChange={this.onChange}
             values={values}
@@ -129,7 +126,6 @@ class ConverterBar extends React.Component {
 class ResultTable extends React.Component {
   calculateSpeedAndPace() {
     const { time, distance } = this.props;
-
     const [hour, minute, second] = time.split(":");
 
     // Convert minutes and seconds to decimal values (30 min = 0.5 h)
@@ -270,226 +266,6 @@ ResultTable.propTypes = {
 ResultTable.defaultProps = {
   time: "",
   distance: 0
-};
-
-/* SLIDER COMPONENTS */
-// *******************************************************
-// SLIDER RAIL (no tooltips)
-// *******************************************************
-const railOuterStyle = {
-  position: "absolute",
-  transform: "translate(0%, -50%)",
-  width: "100%",
-  height: 42,
-  borderRadius: 7,
-  cursor: "pointer"
-  // border: '1px solid grey',
-};
-
-const railInnerStyle = {
-  position: "absolute",
-  width: "100%",
-  height: 14,
-  transform: "translate(0%, -50%)",
-  borderRadius: 7,
-  pointerEvents: "none",
-  backgroundColor: "#f05167"
-};
-
-export function SliderRail({ getRailProps }) {
-  return (
-    <Fragment>
-      <div style={railOuterStyle} {...getRailProps()} />
-      <div style={railInnerStyle} />
-    </Fragment>
-  );
-}
-
-SliderRail.propTypes = {
-  getRailProps: PropTypes.func.isRequired
-};
-
-// *******************************************************
-// HANDLE COMPONENT
-// *******************************************************
-export class Handle extends Component {
-  state = {
-    mouseOver: false
-  };
-
-  onMouseEnter = () => {
-    this.setState({ mouseOver: true });
-  };
-
-  onMouseLeave = () => {
-    this.setState({ mouseOver: false });
-  };
-
-  render() {
-    const {
-      domain: [min, max],
-      handle: { id, value, percent },
-      isActive,
-      disabled,
-      getHandleProps
-    } = this.props;
-    const { mouseOver } = this.state;
-
-    return (
-      <Fragment>
-        {(mouseOver || isActive) && !disabled ? (
-          <div
-            style={{
-              left: `${percent}%`,
-              position: "absolute",
-              marginLeft: "-11px",
-              marginTop: "-35px"
-            }}
-          />
-        ) : null}
-        <div
-          style={{
-            left: `${percent}%`,
-            position: "absolute",
-            transform: "translate(-50%, -50%)",
-            WebkitTapHighlightColor: "rgba(0,0,0,0)",
-            zIndex: 400,
-            width: 26,
-            height: 42,
-            cursor: "pointer",
-            backgroundColor: "none"
-          }}
-          {...getHandleProps(id, {
-            onMouseEnter: this.onMouseEnter,
-            onMouseLeave: this.onMouseLeave
-          })}
-        />
-        <div
-          role="slider"
-          aria-valuemin={min}
-          aria-valuemax={max}
-          aria-valuenow={value}
-          style={{
-            left: `${percent}%`,
-            position: "absolute",
-            transform: "translate(-50%, -50%)",
-            WebkitTapHighlightColor: "rgba(0,0,0,0)",
-            zIndex: 300,
-            width: 24,
-            height: 24,
-            border: 0,
-            borderRadius: "50%",
-            boxShadow: "1px 1px 1px 1px rgba(0, 0, 0, 0.2)",
-            backgroundColor: disabled ? "#666" : "#982736"
-          }}
-        />
-      </Fragment>
-    );
-  }
-}
-
-Handle.propTypes = {
-  domain: PropTypes.array.isRequired,
-  handle: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    value: PropTypes.number.isRequired,
-    percent: PropTypes.number.isRequired
-  }).isRequired,
-  getHandleProps: PropTypes.func.isRequired,
-  isActive: PropTypes.bool.isRequired,
-  disabled: PropTypes.bool
-};
-
-Handle.defaultProps = {
-  disabled: false
-};
-
-// *******************************************************
-// TRACK COMPONENT
-// *******************************************************
-export function Track({ source, target, getTrackProps, disabled }) {
-  return (
-    <div
-      style={{
-        position: "absolute",
-        transform: "translate(0%, -50%)",
-        height: 14,
-        zIndex: 1,
-        backgroundColor: disabled ? "#999" : "#982736",
-        borderRadius: 7,
-        cursor: "pointer",
-        left: `${source.percent}%`,
-        width: `${target.percent - source.percent}%`
-      }}
-      {...getTrackProps()}
-    />
-  );
-}
-
-Track.propTypes = {
-  source: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    value: PropTypes.number.isRequired,
-    percent: PropTypes.number.isRequired
-  }).isRequired,
-  target: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    value: PropTypes.number.isRequired,
-    percent: PropTypes.number.isRequired
-  }).isRequired,
-  getTrackProps: PropTypes.func.isRequired,
-  disabled: PropTypes.bool
-};
-
-Track.defaultProps = {
-  disabled: false
-};
-
-// *******************************************************
-// TICK COMPONENT
-// *******************************************************
-export function Tick({ tick, count, format }) {
-  return (
-    <div>
-      <div
-        style={{
-          position: "absolute",
-          marginTop: 17,
-          width: 1,
-          height: 5,
-          backgroundColor: "rgb(200,200,200)",
-          left: `${tick.percent}%`
-        }}
-      />
-      <div
-        style={{
-          position: "absolute",
-          marginTop: 25,
-          fontSize: 10,
-          textAlign: "center",
-          marginLeft: `${-(100 / count) / 2}%`,
-          width: `${100 / count}%`,
-          left: `${tick.percent}%`
-        }}
-      >
-        {format(tick.value)}
-      </div>
-    </div>
-  );
-}
-
-Tick.propTypes = {
-  tick: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    value: PropTypes.number.isRequired,
-    percent: PropTypes.number.isRequired
-  }).isRequired,
-  count: PropTypes.number.isRequired,
-  format: PropTypes.func.isRequired
-};
-
-Tick.defaultProps = {
-  format: d => d
 };
 
 ReactDOM.render(<PaceCalculator />, document.getElementById("root"));
